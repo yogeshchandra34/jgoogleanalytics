c15576e4fff5d3f9dba3d036141670980f2eba97
siddii <siddii@57d964cf-de47-0410-b657-8f74d426f696>
2008-03-29 15:46:22 +0000

    Minor refactoring

--- src/test/java/com/boxysystems/jgoogleanalytics/JGoogleAnalyticsTracker_UT.java
+++ src/test/java/com/boxysystems/jgoogleanalytics/JGoogleAnalyticsTracker_UT.java
@@ -2,6 +2,8 @@ package com.boxysystems.jgoogleanalytics;
 
 import junit.framework.TestCase;
 
+import java.util.*;
+
 /**
  * Created by IntelliJ IDEA.
  * User: SHAMEED
@@ -11,71 +13,72 @@ import junit.framework.TestCase;
 public class JGoogleAnalyticsTracker_UT extends TestCase {
 
   private TrackPoint parentTrackPoint = new TrackPoint("JGoogleAnalyticsTest");
-  private TrackPoint childFocuPoint = new TrackPoint("TrackingSynchronously", parentTrackPoint);
+  private TrackPoint syncChildFocuPoint = new TrackPoint("TrackingSynchronously", parentTrackPoint);
+  private TrackPoint asyncChildFocuPoint = new TrackPoint("TrackingAsynchronously", parentTrackPoint);
 
-  
-  public void testTrackSynchronously_LibraryFinder() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("LibraryFinder","v1.3.1","UA-2184000-1");
+
+  public void testTrackSynchronously_LibraryFinder() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("LibraryFinder", "v1.3.1", "UA-2184000-1");
     tracker.setVerboseMode(true);
-    tracker.trackSynchronously(childFocuPoint);
+    tracker.trackSynchronously(syncChildFocuPoint);
   }
 
-  public void testTrackAsynchronously_LibraryFinder() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("LibraryFinder","v1.3.1","UA-2184000-1");
+  public void testTrackAsynchronously_LibraryFinder() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("LibraryFinder", "v1.3.1", "UA-2184000-1");
     tracker.setVerboseMode(true);
-    tracker.trackAsynchronously(childFocuPoint);
+    tracker.trackAsynchronously(asyncChildFocuPoint);
     Thread.sleep(3000);
   }
 
-  public void testTrackSynchronously_AOPWorld() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("AOPWorld","UA-941218-1");
+  public void testTrackSynchronously_AOPWorld() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("AOPWorld", "UA-941218-1");
     tracker.setVerboseMode(true);
-    tracker.trackSynchronously(childFocuPoint);
+    tracker.trackSynchronously(syncChildFocuPoint);
   }
 
-  public void testTrackAsynchronously_AOPWorld() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("AOPWorld","UA-941218-1");
+  public void testTrackAsynchronously_AOPWorld() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("AOPWorld", "UA-941218-1");
     tracker.setVerboseMode(true);
-    tracker.trackAsynchronously(childFocuPoint);
+    tracker.trackAsynchronously(asyncChildFocuPoint);
     Thread.sleep(3000);
   }
 
-  public void testTrackSynchronously_TheRandomHomepage() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("TheRandomHomepage.com","UA-941159-1");
+  public void testTrackSynchronously_TheRandomHomepage() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("TheRandomHomepage.com", "UA-941159-1");
     tracker.setVerboseMode(true);
-    tracker.trackSynchronously(childFocuPoint);
+    tracker.trackSynchronously(syncChildFocuPoint);
   }
 
-  public void testTrackAsynchronously_TheRandomHomepage() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("TheRandomHomepage.com","UA-941159-1");
+  public void testTrackAsynchronously_TheRandomHomepage() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("TheRandomHomepage.com", "UA-941159-1");
     tracker.setVerboseMode(true);
-    tracker.trackAsynchronously(childFocuPoint);
+    tracker.trackAsynchronously(asyncChildFocuPoint);
     Thread.sleep(3000);
   }
 
-  public void testTrackSynchronously_Madras_fm() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("Madras.fm","UA-1657019-1");
+  public void testTrackSynchronously_Madras_fm() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("Madras.fm", "UA-1657019-1");
     tracker.setVerboseMode(true);
-    tracker.trackSynchronously(childFocuPoint);
+    tracker.trackSynchronously(syncChildFocuPoint);
   }
 
-  public void testTrackAsynchronously_Madras_fm() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("Madras.fm","UA-1657019-1");
+  public void testTrackAsynchronously_Madras_fm() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("Madras.fm", "UA-1657019-1");
     tracker.setVerboseMode(true);
-    tracker.trackAsynchronously(childFocuPoint);
+    tracker.trackAsynchronously(asyncChildFocuPoint);
     Thread.sleep(3000);
   }
 
-  public void testTrackSynchronously_BoxySystems() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("BoxySystems.com","UA-1657036-1");
+  public void testTrackSynchronously_BoxySystems() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("BoxySystems.com", "UA-1657036-1");
     tracker.setVerboseMode(true);
-    tracker.trackSynchronously(childFocuPoint);
+    tracker.trackSynchronously(syncChildFocuPoint);
   }
 
-  public void testTrackAsynchronously_BoxySystems() throws Exception{
-    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("BoxySystems.com","UA-1657036-1");
+  public void testTrackAsynchronously_BoxySystems() throws Exception {
+    JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker("BoxySystems.com", "UA-1657036-1");
     tracker.setVerboseMode(true);
-    tracker.trackAsynchronously(childFocuPoint);
+    tracker.trackAsynchronously(asyncChildFocuPoint);
     Thread.sleep(3000);
   }
 
