ba8d1b31e1df084b59a5b33b89aabb0d5b736e46
siddii@gmail.com <siddii@gmail.com@57d964cf-de47-0410-b657-8f74d426f696>
2011-01-19 15:44:36 +0000

    Testing browser edit

--- trunk/src/main/java/com/boxysystems/jgoogleanalytics/HostFilter.java
+++ trunk/src/main/java/com/boxysystems/jgoogleanalytics/HostFilter.java
@@ -5,8 +5,7 @@ package com.boxysystems.jgoogleanalytics;
  * User: siddique
  * Date: Jun 23, 2008
  * Time: 5:54:00 PM
- * To change this template use File | Settings | File Templates.
  */
 public interface HostFilter {
-        
+      
 }
